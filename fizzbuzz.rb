174ba65bb3822d9fb68b528817be707712c4088a
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-01 19:58:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(m)
+  s = ""
+  if m % 3 == 0
+    s += "Fizz"
+  end
+  if m % 5 == 0
+    s += "Buzz"
+  end
+  if s.size > 0
+    return s
+  end
+  return nil
+end
